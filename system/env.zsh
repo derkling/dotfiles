00a2680a9144bf7a58f014af4739fd57e8eab7e1
Patrick Bellasi <derkling@gmail.com>
2014-06-03 14:04:38 +0200

    [Env] Configure lesspipe
    Less filter for viewing non text files.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- system/env.zsh
+++ system/env.zsh
@@ -3,3 +3,11 @@ if [[ "$EDITOR" == "" ]] ; then
   # Use vim for my editor.
   export EDITOR='vim'
 fi
+
+# Filter files to less
+if [ -x /usr/bin/lesspipe ]; then
+  [ -n "$ZSH_VERSION" ] && \
+	  eval "$(lesspipe)" # assume Zsh
+  [ -n "$BASH_VERSION" ] && \
+	  "$(SHELL=/bin/sh lesspipe)" #assume Bash
+  fi
