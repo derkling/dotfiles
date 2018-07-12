0cf5b00fd51fca35460046d41d45ad1c5aa93d80
Patrick Bellasi <patrick.bellasi@arm.com>
2018-07-12 14:32:13 +0100

    WIP [Laptop] configuration
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- bin/gnome_keyring.py
+++ bin/gnome_keyring.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python2
 
-import gnomekeyring as gkey
+import keyring as kr
 import argparse
 
 parser = argparse.ArgumentParser(description='GNome Keyring Credentials Proxy')
-parser.add_argument('-u',
-                    dest='get_username',
-                    action='store_const',
-                    const=True,
-                    default=False,
-                    help='return the username')
-parser.add_argument('-p',
-                    dest='get_password',
-                    action='store_const',
-                    const=True,
-                    default=False,
-                    help='return the password')
+parser.add_argument('-s',
+                    nargs=1,
+                    type=str,
+                    help='store the password for the specified username')
+parser.add_argument('-g',
+                    nargs=1,
+                    type=str,
+                    help='get the password for the specified username')
 parser.add_argument('repo',
                     nargs=1,
                     type=str,
@@ -26,28 +22,30 @@ parser.add_argument('username',
                     help='username to store')
 
 def set_credentials(repo, user, pw):
-    KEYRING_NAME = "offlineimap"
+    #KEYRING_NAME = "Mail"
     if isinstance(repo, list):
         repo = repo[0]
     if isinstance(user, list):
         user = user[0]
-    attrs = { "repo": repo, "user": user }
-    keyring = gkey.get_default_keyring_sync()
-    print "Set password in keyring [{}] using {}"\
-           .format(KEYRING_NAME, attrs)
-    gkey.item_create_sync(keyring, gkey.ITEM_NETWORK_PASSWORD,
-        KEYRING_NAME, attrs, pw, True)
+    # attrs = { "repo": repo, "user": user }
+    # keyring = kr.get_default_keyring_sync()
+    # print "Set password in keyring [{}] using {}"\
+    #        .format(KEYRING_NAME, attrs)
+    # kr.item_create_sync(keyring, kr.ITEM_NETWORK_PASSWORD,
+    #     KEYRING_NAME, attrs, pw, True)
+    kr.set_password(repo, user, pw)
 
-def get_credentials(repo):
-    keyring = gkey.get_default_keyring_sync()
-    attrs = {"repo": repo}
-    items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
-    return (items[0].attributes["user"], items[0].secret)
+# def get_credentials(repo):
+#     # keyring = kr.get_default_keyring_sync()
+#     # attrs = {"repo": repo}
+#     # items = kr.find_items_sync(kr.ITEM_NETWORK_PASSWORD, attrs)
+#     # return (items[0].attributes["user"], items[0].secret)
 
-def get_username(repo):
-    return get_credentials(repo)[0]
-def get_password(repo):
-    return get_credentials(repo)[1]
+# def get_username(repo):
+#     return get_credentials(repo)[0]
+def get_password(repo, user):
+    # return get_credentials(repo)[1]
+    return kr.get_password(repo, user)
 
 # NOTE: the content of the keyring can be inspected and manipulated using the
 # "seahorse" GUI under GNOME
@@ -70,16 +68,16 @@ if __name__ == "__main__":
         set_credentials(args.repo, args.username, password)
         sys.exit(0)
 
-    # Get username
-    if args.repo and args.get_username:
-        print get_username(args.repo[0])
-        sys.exit(0)
+    # # Get username
+    # if args.repo and args.get_username:
+    #     print get_username(args.repo[0])
+    #     sys.exit(0)
 
     # Get password
     if args.repo and args.get_password:
         print get_password(args.repo[0])
         sys.exit(0)
 
-    # By default return complete credentials
-    credentials = get_credentials(args.repo[0])
-    print credentials[0], credentials[1]
+    # # By default return complete credentials
+    # credentials = get_credentials(args.repo[0])
+    # print credentials[0], credentials[1]
