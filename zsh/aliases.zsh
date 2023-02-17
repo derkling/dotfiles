cc5e75242a38a2054833cfd639e5b1ef26c06651
Patrick Bellasi <derkling@google.com>
2023-02-17 12:34:53 +0100

    [ZSH] Add emacs related aliases
    Signed-off-by: Patrick Bellasi <derkling@google.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
 alias reload!='. ~/.zshrc'
+alias emacs='emacs -nw'
+
+alias emacs="emacsclient -t" # open file in the current terminal
+export EDITOR="emacsclient -t"
+
+alias emacs-x="emacsclient -c" # open file in a *new* X-Window
+export VISUAL="emacsclient -c"
+
