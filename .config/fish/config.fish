42429716edaec57fdadc399ef8cc7bcaf3702829
Patrick Bellasi <derkling@matbug.net>
2026-04-08 22:50:37 +0200

    [Fish] Fedora Atomic: Teleport for toolbox
    Fedora Atomic systems (Sway, Silverblue, Kinoite) are designed for
    strict filesystem hierarchy standards. On a traditional Linux system,
    /home is its own directory at the root. On Fedora Atomic, /home is
    actually a symbolic link to /var/home.
    
    When Toolbox sets up its container, it resolves those links to the
    "real" physical path to ensure absolute consistency.
    Inside the container, the environment sees the actual mount point.
    
    Add a snippet to ~/.config/fish/config.fish that detects if we are in a
    toolbox and corrects the path.
    
    Signed-off-by: Patrick Bellasi <derkling@matbug.net>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,14 @@
 if status is-interactive
+        # Fedora Atomic: Teleport to the home folder
+        # If we are inside a toolbox...
+        if test -f /run/.containerenv
+            # ... the PWD starts with /var/home
+            if string match -q "/var/home/*" $PWD
+                # Replace /var/home with /home and move there silently
+                cd (string replace "/var/home" "/home" $PWD)
+            end
+        end
+
 	# Use VIM key bindings
 	fish_vi_key_bindings
 	# These bindings are mostly convenience to use the same cords in both
